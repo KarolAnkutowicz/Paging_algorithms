2fdd53366f8f32a0fe0efcabcde4ca45aac078fe
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-05-10 00:09:59 +0200

    Create cPaging class
    Create cPaging class, which will contain "heart" of paging algorithms.

---
+++ Paging_algorithms/cpaging.cpp
@@ -0,0 +1,16 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cpaging.cpp
+ */
+
+#include "cpaging.h"
+
+using namespace std;
+
+cPaging::cPaging()
+{
+}
+
+/* cpaging.cpp */
+/********** END_OF_FILE **********/
---
+++ Paging_algorithms/cpaging.h
@@ -0,0 +1,21 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cpaging.h
+ */
+
+#ifndef CPAGING_H
+#define CPAGING_H
+
+using namespace std;
+
+class cPaging
+{
+public:
+    cPaging();
+};
+
+#endif // CPAGING_H
+
+/* cpaging.h */
+/********** END_OF_FILE **********/
