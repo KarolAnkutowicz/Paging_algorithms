6fb15bdbe3b00d95bd80a5ffce1ad769b150ba28
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-06-01 23:29:39 +0200

    Add fields in cPaging
    Add definitions of fields in cPaging class.

--- Paging_algorithms/cpaging.h
+++ Paging_algorithms/cpaging.h
@@ -7,11 +7,60 @@
 #ifndef CPAGING_H
 #define CPAGING_H
 
+#include "cdata.h"
+#include "constantsandtypes.h"
+
 using namespace std;
 
 class cPaging
 {
 /********** PRIVATE: BEGINNING **********/
+
+    /*
+     * cData D - zestaw danych wykorzystywanych w algorytmach
+     * stronicowania.
+     */
+    cData D;
+
+    /*
+     * tableFrame[constFrame] - tablica ramek.
+     */
+    typePaging tableFrame[constFrame];
+
+    /*
+     * tableNumberOfLack[constSeries] - tablica brakow
+     * ramek.
+     */
+    typePaging tableNumberOfLack[constSeries];
+
+    /*
+     * vAverageNumberOfLack - srednia wartosc brakow ramek.
+     */
+    double vAverageNumberOfLack;
+
+
+
+    /*
+     * tableTypingOrder[constFrame] - tablica ramek
+     * wytypowanych do zastapienia.
+     */
+    typePaging tableTypingOrder[constFrame];
+
+
+
+    /*
+     * tableTimeSchronology[constFrame] - tablica
+     * kolejnosci chronologicznej stron w ramkach.
+     */
+    typePaging tableTimeSchronology[constFrame];
+
+
+
+    /*
+     * tablePage[constPage] - tablica uzycia poszczegolnych
+     * stron.
+     */
+    typePaging tablePage[constPage];
 
 /********** PRIVATE: END **********/
 
