dbd17db4994fb6542bf5cf7dfd50e3454a63b8e1
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-07-09 22:49:38 +0200

    Expansion cPagingAlgorithms class
    Start programming methods in cPagingAlgorithms class.

--- Paging_algorithms/cpagingalgorithms.cpp
+++ Paging_algorithms/cpagingalgorithms.cpp
@@ -10,8 +10,198 @@ using namespace std;
 
 /********** PUBLIC: BEGINNING **********/
 
+/*
+ * cPagingAlgorithms()
+ */
 cPagingAlgorithms::cPagingAlgorithms()
 {
+
+}
+
+/*
+ * cPagingAlgorithms(enumAlgorithms aAlgorithm)
+ */
+cPagingAlgorithms::cPagingAlgorithms(enumAlgorithms aAlgorithm)
+{
+
+}
+
+
+
+/*
+ * void mMakeFIFO()
+ */
+void cPagingAlgorithms::mMakeFIFO()
+{
+
+}
+
+/*
+ * void mMakeLRU()
+ */
+void cPagingAlgorithms::mMakeLRU()
+{
+
+}
+
+/*
+ * void mMakeLFU()
+ */
+void cPagingAlgorithms::mMakeLFU()
+{
+
+}
+
+/*
+ * void mMakeMFU()
+ */
+void cPagingAlgorithms::mMakeMFU()
+{
+
+}
+
+/*
+ * void mMakePage(typePaging aSeries, typePaging aIndex)
+ */
+void cPagingAlgorithms::mMakePage(typePaging aSeries, typePaging aIndex)
+{
+
+}
+
+/*
+ * typePaging mGetTheOldestPage()
+ */
+typePaging cPagingAlgorithms::mGetTheOldestPage()
+{
+    return 0;
+}
+
+/*
+ * typePaging mGetLeastFrequentlyUsed()
+ */
+typePaging cPagingAlgorithms::mGetLeastFrequentlyUsed()
+{
+    return 0;
+}
+
+/*
+ * typePaging mGetMostFrequentlyUsed()
+ */
+typePaging cPagingAlgorithms::mGetMostFrequentlyUsed()
+{
+    return 0;
+}
+
+
+
+/*
+ * void mDrawReferences()
+ */
+void cPagingAlgorithms::mDrawReferences()
+{
+
+}
+
+/*
+ * void mResetSeries(typePaging aSeries)
+ */
+void cPagingAlgorithms::mResetSeries(typePaging aSeries)
+{
+
+}
+
+/*
+ * void mResetAllReferences()
+ */
+void cPagingAlgorithms::mResetAllReferences()
+{
+
+}
+
+/*
+ * void mReadReferencesFromFile()
+ */
+void cPagingAlgorithms::mReadReferencesFromFile()
+{
+
+}
+
+/*
+ * void mWriteReferencesToFile()
+ */
+void cPagingAlgorithms::mWriteReferencesToFile()
+{
+
+}
+
+/*
+ * void mPrintSeries(typePaging aSeries)
+ */
+void cPagingAlgorithms::mPrintSeries(typePaging aSeries)
+{
+
+}
+
+/*
+ * void mPrintAllReferences()
+ */
+void cPagingAlgorithms::mPrintAllReferences()
+{
+
+}
+
+
+
+/*
+ * void mResetFrames()
+ */
+void cPagingAlgorithms::mResetFrames()
+{
+
+}
+
+/*
+ * void mPrintFrames()
+ */
+void cPagingAlgorithms::mPrintFrames()
+{
+
+}
+
+
+
+/*
+ * void mResetSumNumberOfLack(typePaging aSeries)
+ */
+void cPagingAlgorithms::mResetSumNumberOfLack(typePaging aSeries)
+{
+
+}
+
+/*
+ * void mResetAllSumNumberOfLack()
+ */
+void cPagingAlgorithms::mResetAllSumNumberOfLack()
+{
+
+}
+
+/*
+ * void mCalculateTotalNumberOfLacks()
+ */
+void cPagingAlgorithms::mCalculateTotalNumberOfLacks()
+{
+
+}
+
+
+
+/*
+ * void mWriteResultsToFile(enumAlgorithms aAlgorithms)
+ */
+void cPagingAlgorithms::mWriteResultsToFile(enumAlgorithms aAlgorithms)
+{
+
 }
 
 /********** PUBLIC: END **********/
