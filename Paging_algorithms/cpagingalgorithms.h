492b5c30a2e7efc138120d43c3b943f2e65e9be8
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-07-09 08:25:53 +0200

    Expansion cPagingAlgorithms
    Add probably all declarations of fields and methods in cPagingAlgorithms class.

--- Paging_algorithms/cpagingalgorithms.h
+++ Paging_algorithms/cpagingalgorithms.h
@@ -36,6 +36,23 @@ class cPagingAlgorithms
      */
     cPage tabFrames[constFrame];
 
+    /*
+     * double tabAverageNumberOfLack[constSeries] - tablica
+     * sum brakow stron w poszczegolnych seriach.
+     */
+    typePaging tabSumNumberOfLack[constSeries];
+
+    /*
+     * typePaging tabTotalNumberOfLacks - calkowita suma
+     * brakow stron.
+     */
+    typePaging tabTotalNumberOfLacks;
+
+    /*
+     * double vAverageNumberOfLack - srednia wartosc brakow stron.
+     */
+    double vAverageNumberOfLack;
+
 /********** PRIVATE: END **********/
 
 /********** PUBLIC: BEGINNING **********/
@@ -59,8 +76,10 @@ public:
      */
     cPagingAlgorithms(enumAlgorithms aAlgorithm);
 
+
+
     /*
-     *  void mMakeFIFO() - metoda realizujaca algorytm FIFO.
+     * void mMakeFIFO() - metoda realizujaca algorytm FIFO.
      * PRE:
      * - brak;
      * POST:
@@ -94,6 +113,237 @@ public:
      * - brak.
      */
     void mMakeMFU();
+
+    /*
+     * void mMakePage(typePaging aSeries, typePaging aIndex) -
+     * metoda wykonujaca wskazana strone.
+     * PRE:
+     * - podanie numeru serii (typ: typePaging), podanie
+     * numeru referencji (typ: typePaging);
+     * POST:
+     * - brak.
+     */
+    void mMakePage(typePaging aSeries, typePaging aIndex);
+
+    /*
+     * typePaging mGetTheOldestPage() - metoda znajdujaca
+     * i zwracajaca najdawniej uzywana strone.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie numeru strony sposrod stron znajdujacych sie
+     * w ramkach (typ: typePaging).
+     */
+    typePaging mGetTheOldestPage();
+
+    /*
+     * typePaging mGetLeastFrequentlyUsed() - metoda znajdujaca
+     * i zwracajaca najrzadziej uzywana strone.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie numeru strony sposrod stron znajdujacych sie
+     * w ramkach (typ: typePaging).
+     */
+    typePaging mGetLeastFrequentlyUsed();
+
+    /*
+     * typePaging mGetMostFrequentlyUsed() - metoda znajdujaca
+     * i zwracajaca naczesciej uzywana strone.
+     * PRE:
+     * - brak;
+     * POST:
+     * - zwrocenie numeru strony sposrood stron znajdujacych sie
+     * w ramkach (typ: typePaging).
+     */
+    typePaging mGetMostFrequentlyUsed();
+
+
+
+    /*
+     * void mDrawReferences() - metoda losujaca referencje.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mDrawReferences();
+
+    /*
+     * void mResetReference(typePaging aSeries, typePaging aReference) -
+     * metodda resetujaca wskazana referencje.
+     * PRE:
+     * - podanie numeru serii (typ: typePaging), podanie numeru
+     * referencji (typ: typePaging);
+     * POST:
+     * - brak.
+     */
+    void mResetReference(typePaging aSeries, typePaging aReference);
+
+    /*
+     * void mResetSeries(typePaging aSeries) - metoda resetujaca
+     * wskazana serie referencji.
+     * PRE:
+     * - podanie numeru serii (typ: typePaging);
+     * POST:
+     * - brak.
+     */
+    void mResetSeries(typePaging aSeries);
+
+    /*
+     * void mResetAllReferences() - metoda resetujaca wszystkie
+     * referencje.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mResetAllReferences();
+
+    /*
+     * void mReadReferencesFromFile() - metoda wczytujaca referencje
+     * z pliku.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mReadReferencesFromFile();
+
+    /*
+     * void mWriteReferencesToFile() - metoda wypisujaca referencje
+     * do pliku.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mWriteReferencesToFile();
+
+    /*
+     * void mPrintReference(typePaging aSeries, typePaging aReference) -
+     * metoda wypisujaca wskazana referencje.
+     * PRE:
+     * - podanie numeru serii (typ: typePaging), podanie numeru
+     * referencji (typ: typePaging);
+     * POST:
+     * - brak.
+     */
+    void mPrintReference(typePaging aSeries, typePaging aReference);
+
+    /*
+     * void mPrintSeries(typePaging aSeries) - metoda wypisujaca
+     * wskazana serie referencji.
+     * PRE:
+     * - podanie numeru serii (typ: typePaging);
+     * POST:
+     * - brak.
+     */
+    void mPrintSeries(typePaging aSeries);
+
+    /*
+     * void mPrintAllReferences() - metoda wypisujaca wszystkie
+     * referencje.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintAllReferences();
+
+
+
+    /*
+     * void mResetFrames() - metoda resetujaca ramki.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mResetFrames();
+
+    /*
+     * void mPrintFrames() - metoda wypisujaca zawartosc ramek.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mPrintFrames();
+
+
+
+    /*
+     * void mResetSumNumberOfLack(typePaging aSeries) - metoda
+     * resetujaca wskazana sume brakow stron.
+     * PRE:
+     * - podanie numeru serii (typ: typePaging);
+     * POST:
+     * - brak.
+     */
+    void mResetSumNumberOfLack(typePaging aSeries);
+
+    /*
+     * void mResetAllSumNumberOfLack() - metoda resetujaca
+     * wszystkie sumy brakow stron.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mResetAllSumNumberOfLack();
+
+    /*
+     * void mCalculateTotalNumberOfLacks() - metoda wyliczajaca
+     * calkowita sumer brakow stron.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mCalculateTotalNumberOfLacks();
+
+    /*
+     * void mResetTotalNumberOfLacks() - metoda resetujaca
+     * calkowita sume brakow stron.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mResetTotalNumberOfLacks();
+
+    /*
+     * void mCalculateAverageNumberOfLacks() - metoda obliczajaca
+     * srednia liczba brakow stron.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mCalculateAverageNumberOfLacks();
+
+    /*
+     * void mResetAverageNumberOfLacks() - metoda resetujaca
+     * srednia liczba brakow stron.
+     * PRE:
+     * - brak;
+     * POST:
+     * - brak.
+     */
+    void mResetAverageNumberOfLacks();
+
+
+
+    /*
+     * void mWriteResultsToFile(enumAlgorithms aAlgorithms) -
+     * metoda wypisujaca wyniki dzialan wskazanego algorytmu.
+     * PRE:
+     * - podanie kodu algorytmu (typ: enumAlgorithms);
+     * POST:
+     * - brak.
+     */
+    void mWriteResultsToFile(enumAlgorithms aAlgorithms);
 
 /********** PUBLIC: END **********/
 };
